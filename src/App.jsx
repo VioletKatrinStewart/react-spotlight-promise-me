cc1dfd67d99ba72608bcdb39df5fa057bf8c2785
Violet Stewart <vkazoo2@gmail.com>
2022-01-26 13:30:27 -0800

    API calls are working

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,23 @@
+import React, { useEffect } from "react";
+import { fetchBrews, getBrews, getFilms, fetchFilms } from "./services/promise-me";
+
+
 export default function App() {
+  useEffect(() => {
+    fetchFilms();
+  });
+
+  useEffect(() => {
+    getFilms();
+  });
+
+  useEffect(() => {
+    fetchBrews();
+  });
+
+  useEffect(() => {
+    getBrews();
+  });
+
   return <h1>Hello World</h1>;
 }
--- src/services/promise-me.js
+++ src/services/promise-me.js
@@ -1,3 +1,31 @@
+import { response } from 'msw'
+
+export async function getFilms() {
+    const resp = await fetch('https://ghibliapi.herokuapp.com/films');
+    const data = await resp.json();
+    return console.log(data)
+}
+
+export const fetchFilms = () => {
+    const response = fetch('https://ghibliapi.herokuapp.com/films')
+    .then((data) => data.json())
+    .then((films) => console.log(films));
+    return response;
+};
+
+export async function getBrews() {
+    const resp = await fetch('https://api.openbrewerydb.org/breweries');
+    const data = await resp.json();
+    return console.log(data);
+}
+
+export const fetchBrews = () => {
+    const response = fetch('https://api.openbrewerydb.org/breweries')
+    .then((data) => data.json())
+    .then((breweries) => console.log(breweries));
+    return response;
+};
+
 /**
  * TODO:
  */
